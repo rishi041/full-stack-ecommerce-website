a62c2ebed056e9f197263947216d878a887a17a8
rushikeshg <you@example.com>
2023-10-15 18:35:32 +0530

    jwt token added Oct15

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
-    "postgres://vfsksgtw:mTp6pCmtCLx2AOfH9-qCEMfdQpOnRnhr@rajje.db.elephantsql.com/vfsksgtw",
+    "",
   ssl: process.env.DATABASE_URL ? true : false,
 });
 
--- server.js
+++ server.js
@@ -2,20 +2,12 @@ const express = require("express");
 const studentRoutes = require("./src/student/routes");
 const productRoutes = require("./src/products/routes");
 const usersRoutes = require("./src/users/routes");
-
 const cors = require("cors");
-
+const port = 4000;
 const app = express();
 
-app.use(cors());
-
-const port = 4000;
-
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
 
 app.use("/api/v1/students", studentRoutes);
 app.use("/api/v1/products", productRoutes);
